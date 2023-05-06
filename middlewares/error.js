e09eb7edfcf27d13a5099f2238e08b7df6efd1c6
Aman00015 <115334661+Aman00015@users.noreply.github.com>
2023-05-06 20:07:18 +0530

    Fixed Err Issue

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,17 +1,18 @@
-class ErrorHandler extends Error{
-    constructor(message,statusCode){
-        super(message)
-        this.statusCode = statusCode
+class ErrorHandler extends Error {
+    constructor(message, statusCode) {
+      super(message);
+      this.statusCode = statusCode;
     }
-}
-export const errorMiddleware=()=>{
-    err.message = err.message || "Internal server Error"
-    err.message = err.statusCode || 500
-    return (err,req,res,next)=>{
-         res.status(err.statusCode).json({
-          success:false,
-          message:err.message,
-      })
-      }
-}
-export default ErrorHandler
+  }
+  
+  export const errorMiddleware = (err, req, res, next) => {
+    err.message = err.message || "Internal Server Error";
+    err.statusCode = err.statusCode || 500;
+  
+    return res.status(err.statusCode).json({
+      success: false,
+      message: err.message,
+    });
+  };
+  
+  export default ErrorHandler;
