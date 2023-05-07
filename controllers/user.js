4d0df7fb24c1ae0fe12c708b7c6e70b099d06f17
Aman00015 <115334661+Aman00015@users.noreply.github.com>
2023-05-07 19:45:34 +0530

    Fixed Typo

--- controllers/user.js
+++ controllers/user.js
@@ -3,60 +3,71 @@ import bcrypt from "bcrypt";
 import { sendCookie } from "../utils/features.js";
 import ErrorHandler from "../middlewares/error.js";
 
-export const login = async (req, res, next) => {
+export const getAllUsers = async (req, res) => {};
+
+export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
-
     const user = await User.findOne({ email }).select("+password");
-
-    if (!user) return next(new ErrorHandler("Invalid Email or Password", 400));
+    if (!user) {
+      return next(new ErrorHandler("invalid Email or password", 400));
+    }
 
     const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return next(new ErrorHandler("invalid email or password", 400));
+    }
 
-    if (!isMatch)
-      return next(new ErrorHandler("Invalid Email or Password", 400));
-
-    sendCookie(user, res, `Welcome back, ${user.name}`, 200);
+    sendCookie(user, res, `Welcome back ${user.name}`, 200);
   } catch (error) {
     next(error);
   }
 };
-
-export const register = async (req, res) => {
+export const register = async (req, res,next) => {
+  const { name, email, password } = req.body;
   try {
-    const { name, email, password } = req.body;
-
     let user = await User.findOne({ email });
-
-    if (user) return next(new ErrorHandler("User Already Exist", 400));
+    if (user) {
+      return next(new ErrorHandler("User already Exist", 400));
+    }
 
     const hashedPassword = await bcrypt.hash(password, 10);
-
     user = await User.create({ name, email, password: hashedPassword });
 
-    sendCookie(user, res, "Registered Successfully", 201);
+    sendCookie(user, res, "Registered Successful", 201);
   } catch (error) {
     next(error);
   }
 };
 
-export const getMyProfile = (req, res) => {
-  res.status(200).json({
-    success: true,
-    user: req.user,
-  });
-};
+export const getMyProfile = async (req, res) => {
+  try {
+    const id = "myid";
 
-export const logout = (req, res) => {
-  res
-    .status(200)
-    .cookie("token", "", {
-      expires: new Date(Date.now()),
-      sameSite: process.env.NODE_ENV === "Develpoment" ? "lax" : "none",
-      secure: process.env.NODE_ENV === "Develpoment" ? false : true,
-    })
-    .json({
+    res.status(200).json({
       success: true,
       user: req.user,
     });
+  } catch (error) {
+    next(error);
+  }
 };
+export const logout = async (req, res) => {
+  try {
+    res
+      .status(200)
+      .cookie("token", "", {
+        expires: new Date(Date.now()),
+        sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",
+        secure: process.env.NODE_ENV === "development" ? false : true,
+      })
+      .json({
+        success: true,
+        message: "logout successful",
+        user: req.user,
+      });
+  } catch (error) {
+    next(error);
+  }
+};
+// 5:10
